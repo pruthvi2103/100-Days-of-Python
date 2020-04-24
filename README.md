e0129e90891a0a9fe9bb8e81b6c2b3898b3c7012
pruthvi2103 <pruthvishetty5656@gmail.com>
2020-04-25 00:17:21 +0530

    Added the pre order traversal

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # 100-Days-of-Python
 #### From 23-04-2020 I will be commiting myself to 1 new python concept each day, it can range from basic Regex, Data Structures,Algorithms to full fledged mini apps/games but doing it consistently for 100 days 
 
-##### 23-04-2020: Day 1
+##### 23-04-2020: Day 1 : Binary trees and insertion
+
+#### 24-04-2020: Day 2 : 
--- binaary_tree.py
+++ binaary_tree.py
@@ -1,5 +1,14 @@
 #binaary tree
 #my own take on a binary tree
+# Class Node defines a specific node, having properties: 
+#   value: which has the value stored in each node
+#   leftNode:   which has the node stored on the left
+#   rightNode:  which has the node stored on the Right
+# 
+# Class BinaryTree is the main binary tree 
+#   it has input method for input
+#   and preorder method denotes Preorder traversal and prints the pre order route
+#  
 class Node(object):
         def __init__(self,value):
                 self.value = value
@@ -11,45 +20,60 @@ class BinaryTree():
     def __init__(self,root):
         self.root=Node(root)
 
-
-# Insert function
-def BTreeinsert(node,val):
-    if node.value < val:
-        if node.rightNode == None:
-            node.rightNode = Node(val)
+    def print_tree(self, traversal_type):
+        
+        if traversal_type == "preorder":
+            return self.preorder_print(tree.root,"")
+    # Insert function
+    def insert(self,node,val):
+        if node.value < val:
+            if node.rightNode == None:
+                node.rightNode = Node(val)
           #  print("inserted right")
-        else:
+            else:
             
-            BTreeinsert(node.rightNode,val)
+                self.insert(node.rightNode,val)
     
 
-    if node.value > val:
-        if node.leftNode == None:
-            node.leftNode = Node(val)
+        if node.value > val:
+            if node.leftNode == None:
+                node.leftNode = Node(val)
            # print("inserted left")
-        else:
+            else:
             
-            BTreeinsert(node.leftNode,val)
+                self.insert(node.leftNode,val)
+
+    def preorder_print(self,start,traversal):
+        if start:
+            traversal += (str(start.value) + "->")
+            traversal = self.preorder_print(start.leftNode,traversal)
+            traversal = self.preorder_print(start.rightNode,traversal)
+        return traversal
 
 #def BTreeprint(node):
 
     
-
-inp=input("Enter comma seperated values to be converted to binary tree").split(',')
+# driver code
+#def main() :
+inp=input("Enter comma seperated values to be converted to binary tree:  ").split(',')
 
 tree = BinaryTree(inp[0])
 inp.pop(0)
-print(tree.root.value)
+# print(tree.root.value)
 
 for x in inp:
     spec = tree.root
-    BTreeinsert(spec,x)
-print(tree.root.leftNode.rightNode.value)
+    tree.insert(spec,x)
+#print(tree.root.leftNode.rightNode.value)
+print(tree.print_tree("preorder").rstrip('->'))
         
+
+#if __name__ == "__main__":
+#    main()
 
          
 
-
+# Made by Pruthvi Shetty as a part of the 100 days of python challange  on 23-04-2020
 
 
 
