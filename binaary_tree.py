a7cd1532f76f3394efc92c48090c4cf35c5d38ef
pruthvi2103 <pruthvishetty5656@gmail.com>
2020-04-24 19:03:14 +0530

    added insert function
    added insert function

--- binaary_tree.py
+++ binaary_tree.py
@@ -11,15 +11,46 @@ class BinaryTree():
     def __init__(self,root):
         self.root=Node(root)
 
+
+# Insert function
+def BTreeinsert(node,val):
+    if node.value < val:
+        if node.rightNode == None:
+            node.rightNode = Node(val)
+          #  print("inserted right")
+        else:
+            
+            BTreeinsert(node.rightNode,val)
+    
+
+    if node.value > val:
+        if node.leftNode == None:
+            node.leftNode = Node(val)
+           # print("inserted left")
+        else:
+            
+            BTreeinsert(node.leftNode,val)
+
+#def BTreeprint(node):
+
+    
+
 inp=input("Enter comma seperated values to be converted to binary tree").split(',')
 
 tree = BinaryTree(inp[0])
 inp.pop(0)
+print(tree.root.value)
 
-print (tree.root.value + 1)
+for x in inp:
+    spec = tree.root
+    BTreeinsert(spec,x)
+print(tree.root.leftNode.rightNode.value)
+        
 
-# for x in tree:
-#     if x > int(tree.root.value):
+         
+
+
+
 
 
 
